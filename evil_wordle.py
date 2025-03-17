523a18c0272c7f0fabb0f973e4f67dc3431d3d15
Kashif Ghani <143444185+khghani@users.noreply.github.com>
2025-03-16 23:12:58 -0500

    Add files via upload

--- evil_wordle.py
+++ evil_wordle.py
@@ -178,9 +178,9 @@ class WordFamily:
         if not isinstance(other, WordFamily):
             raise NotImplementedError("< operator only valid for WordFamily comparisons.")
         if len(self.words) != len(other.words):
-            return len(self.words) < len(other.words)
+            return len(self.words) > len(other.words)
         if self.difficulty != other.difficulty:
-            return self.difficulty < other.difficulty
+            return self.difficulty > other.difficulty
         return self.feedback_colors < other.feedback_colors
         return False
 
